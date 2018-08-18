abcdf8a4b897015bfaabd0a60c77579ac51c6c79
kskfjaosfuahfiyatfiusajfsajhfihdsgf <42489061+kskfjaosfuahfiyatfiusajfsajhfihdsgf@users.noreply.github.com>
2018-08-18 11:13:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,3 +29,32 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
 });
 
 client.login(process.env.BOT_TOKEN);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on("guildMemberAdd", function(member) {
+    const wc = member.guild.channels.find("name", "member-log")
+        const embed = new Discord.RichEmbed()
+        .setColor('B90C0C')
+        .setAuthor(member.user.tag, member.user.avatarURL)
+ .setDescription('***يا مرحبا وسهلاً بضيف لفانا، يزهي بك الأدب العربي وينثر لك أزهار يسقيك من نبع المشاعر وفانا، لين الهلا تثمر على غصونك أطيار. ***')
+.setThumbnail(member.avatarURL)
+  .setImage('https://www.askideas.com/media/13/Welcome-Deers-Sign.jpg')
+        .setTimestamp()
+        return wc.sendEmbed(embed);
+        
+});
