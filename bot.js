23290340307cdbc2d13f452cf2ac74382114b21f
kskfjaosfuahfiyatfiusajfsajhfihdsgf <42489061+kskfjaosfuahfiyatfiusajfsajhfihdsgf@users.noreply.github.com>
2018-08-18 11:26:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,25 +72,3 @@ client.on("guildMemberAdd", function(member) {
 
 
 
-const devs = ['430038396585377793' , '' , '' , ''];
-const adminprefix = "#";
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/حب بلا حدود");
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  }
