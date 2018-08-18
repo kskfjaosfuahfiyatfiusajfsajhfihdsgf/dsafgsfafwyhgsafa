85cb30d45ef6c0bf9b89bd072ba8e9377ab4f258
kskfjaosfuahfiyatfiusajfsajhfihdsgf <42489061+kskfjaosfuahfiyatfiusajfsajhfihdsgf@users.noreply.github.com>
2018-08-18 11:31:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,29 @@ client.login(process.env.BOT_TOKEN);
 
 
 
-
+const devs = ['430038396585377793 , '' , '' , ''];
+const adminprefix = "#";
+client.on('message', message => {
+    var argresult = message.content.split(`).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(:white_check_mark:   ${argresult})
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(:white_check_mark:   ${argresult})
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(:white_check_mark:   ${argresult})
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/حب بلا حدود");
+      message.channel.sendMessage(:white_check_mark:   ${argresult}`)
+  }
+  });
 
 
 
